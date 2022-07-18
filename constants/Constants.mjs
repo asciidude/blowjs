7e18d779f142d5e7a8194ec19998d092a3eb0473
asciidude <sickspamemail@gmail.com>
2022-07-17 20:07:34 -0500

    Switch to v1 again.. v2 wont work

--- constants/Constants.mjs
+++ constants/Constants.mjs
@@ -4,7 +4,7 @@ export const Constants = {
     WS_URL: 'wss://ws.bubblez.app/live',
     API_URL: null,
     CANARY_URL: 'wss://ws.bubblez.app/canary',
-    API_VERSION: 2
+    API_VERSION: 1
 }
 
 Constants.API_URL = `https://bubblez.app/api/v${Constants.API_VERSION}`
